e406ebac92a6496751b24a189c0c37d33c663c02
Kelly <kslimmo@ucls.uchicago.edu>
2016-04-28 09:59:02 -0500

    final commit

--- src/kelly/KellyNetBeansController.java
+++ src/kelly/KellyNetBeansController.java
@@ -14,8 +14,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
 
 /**
  *
@@ -25,12 +30,36 @@ public class KellyNetBeansController implements Initializable {
     
     @FXML
     private BarChart barChart;
+    
     @FXML
     private Slider minSlider;
     
     @FXML
     private Slider maxSlider;
     
+    @FXML
+    private MenuBar menuBar;
+    
+    @FXML
+    private void handleChangeFilter(MouseEvent event) {
+        barChart.getData().clear();
+        initialize(null, null);
+    }
+    
+    @FXML
+    private void selectFile(ActionEvent close) {
+        System.exit(0);
+    }
+    
+    @FXML
+    private void selectHelp(ActionEvent about) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Information Dialog");
+        alert.setHeaderText("Kelly Slimmon");
+        alert.setContentText("This is data showing the percent of polio immunizations by country.");
+
+        alert.showAndWait();
+    }
     
     
     @Override
@@ -68,7 +97,8 @@ public class KellyNetBeansController implements Initializable {
         for (Fact blah1 : blah) {
             if(blah1.getDim().getCountry() == null){
                 
-            }else{
+            }else if(blah1.getValue() <= maxSlider.getValue() && minSlider.getValue() <= blah1.getValue()){
+                
             dataInfo.getData().add(new XYChart.Data(blah1.getDim().getCountry(), blah1.getValue()));
             }
         }
